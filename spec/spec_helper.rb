d3535a27c25b23b3698ca7acaf4c4309d3a0f72b
Sidney Liebrand <sidneyliebrand@gmail.com>
2018-06-21 00:53:41 +0200

    Small tweaks

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,18 +6,25 @@ require 'faker'
 require 'simple_csv'
 
 module Helpers
-  HEADERS = [:first_name, :last_name, :birth_date, :employed_at].freeze
+  HEADERS = [:first_name, :last_name, :age, :birth_date, :employed_at].freeze
 
   def self.generate_csv(path, **options)
     n = options.delete(:rows) || 1000
-    SimpleCsv.generate('spec/files/output.csv', options) do
+    fn = options.delete(:first_name) || 'foo'
+    ln = options.delete(:last_name) || 'bar'
+    bd = options.delete(:birth_date) || (Date.today << 1000)
+    ea = options.delete(:employed_at) || ['bizz', nil].sample
+    ag = options.delete(:age) || 20
+
+    SimpleCsv.generate(path, options) do
       headers(*HEADERS)
 
       n.times do
-        first_name 'foo'
-        last_name 'bar'
-        birth_date (Date.today << 1000)
-        employed_at ['bizz', nil].sample
+        first_name fn
+        last_name ln
+        age ag
+        birth_date bd
+        employed_at ea
       end
     end
 
